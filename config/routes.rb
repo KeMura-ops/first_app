61263dad29b1f7f0c3bf83ec196d73a7194f773e
KeMura-ops <kenti432@yahoo.co.jp>
2021-06-01 17:50:46 +0900

    同期通信でのいいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
   get 'posts/:id', to: 'posts#checked'
+  post 'like/:id' => 'likes#create', as: 'create_like'
+  delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 end
